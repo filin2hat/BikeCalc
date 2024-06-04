898797131465cab0b9b55389b9abac9d91412781
Dmitrii Biryulin <biryulin.develop@gmail.com>
2024-06-04 20:59:22 +0300

    Изменил структуру файлов. Добавил расчет давления для шоссеров.

--- app/src/main/java/dev/filinhat/bikepressurecalc/common/EmpiricalCoefficient.kt
+++ app/src/main/java/dev/filinhat/bikepressurecalc/common/EmpiricalCoefficient.kt
@@ -1,5 +1,10 @@
 package dev.filinhat.bikepressurecalc.common
 
+/**
+ * K — эмпирический коэффициент, который можно определить на основании опыта и
+ * различных тестов. Обычно для MTB велосипедов его значение находится в диапазоне от 50 до 100.
+ * Для шоссейных велосипедов его значение находится в диапазоне от 100 до 150.
+ */
 const val mtbFrontEmpiricalCoefficient = 71
 const val mtbRearEmpiricalCoefficient = 65
 const val roadFrontEmpiricalCoefficient = 120
