fe3fc6bc9eb2144788ae6fd5ef2a81fbd6863fc3
Dmitrii Biryulin <biryulin.develop@gmail.com>
2024-07-23 06:23:45 +0300

    Реализована базовая ViewModel

--- app/src/main/kotlin/dev/filinhat/bikepressurecalc/presentation/screen/PressureCalculatorViewModel.kt
+++ app/src/main/kotlin/dev/filinhat/bikepressurecalc/presentation/screen/PressureCalculatorViewModel.kt
@@ -3,10 +3,25 @@ package dev.filinhat.bikepressurecalc.presentation.screen
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.filinhat.bikepressurecalc.data.repository.PressureCalcRepository
+import dev.filinhat.bikepressurecalc.presentation.screen.PressureCalculatorViewModel.UiEvent
+import dev.filinhat.bikepressurecalc.presentation.screen.PressureCalculatorViewModel.UiState
+import dev.filinhat.bikepressurecalc.presentation.util.BaseViewModel
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class PressureCalculatorViewModel @Inject constructor(
     private val repository: PressureCalcRepository
-) : ViewModel() {
+) : BaseViewModel<UiState, UiEvent> ,ViewModel() {
+
+    sealed interface UiState{}
+
+    sealed interface UiEvent{}
+
+    override val uiState: StateFlow<UiState>
+        get() = TODO("Not yet implemented")
+
+    override fun perform(event: UiEvent) {
+        TODO("Not yet implemented")
+    }
 }
