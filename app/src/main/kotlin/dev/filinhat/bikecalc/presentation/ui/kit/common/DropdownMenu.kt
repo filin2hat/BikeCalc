d541d5d5c85a9dfb0ab6b1070730d6dc3547314f
Dmitriy Biryulin <biryulin.develop@gmail.com>
2025-01-14 09:33:12 +0300

    Обновление зависимостей, мелкие исправления

--- app/src/main/kotlin/dev/filinhat/bikecalc/presentation/ui/kit/common/DropdownMenu.kt
+++ app/src/main/kotlin/dev/filinhat/bikecalc/presentation/ui/kit/common/DropdownMenu.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -71,18 +72,21 @@ fun <T> DropdownMenu(
                 },
                 textStyle = MaterialTheme.typography.bodyLarge,
                 colors =
-                    ExposedDropdownMenuDefaults.outlinedTextFieldColors(
-                        focusedContainerColor = MaterialTheme.colorScheme.background,
-                        unfocusedContainerColor = MaterialTheme.colorScheme.background,
-                        focusedTextColor = MaterialTheme.colorScheme.primary,
-                        unfocusedTextColor = MaterialTheme.colorScheme.onBackground,
-                        unfocusedBorderColor = MaterialTheme.colorScheme.inversePrimary,
-                    ),
+                ExposedDropdownMenuDefaults.outlinedTextFieldColors(
+                    focusedContainerColor = MaterialTheme.colorScheme.background,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.background,
+                    focusedTextColor = MaterialTheme.colorScheme.primary,
+                    unfocusedTextColor = MaterialTheme.colorScheme.onBackground,
+                    unfocusedBorderColor = MaterialTheme.colorScheme.inversePrimary,
+                ),
                 shape = MaterialTheme.shapes.medium,
                 modifier =
-                    Modifier
-                        .menuAnchor()
-                        .fillMaxWidth(),
+                Modifier
+                    .fillMaxWidth()
+                    .menuAnchor(
+                        type = MenuAnchorType.PrimaryNotEditable,
+                        enabled = true
+                    )
             )
             ExposedDropdownMenu(
                 expanded = isMenuExpanded,
@@ -105,9 +109,9 @@ fun <T> DropdownMenu(
                             }
                         },
                         colors =
-                            MenuDefaults.itemColors(
-                                textColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                            ),
+                        MenuDefaults.itemColors(
+                            textColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                        ),
                     )
                 }
             }
@@ -130,9 +134,9 @@ private fun PreviewDropdownMenu() {
             onItemSelected = { },
             label = "Label",
             modifier =
-                Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth(),
+            Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
             value = "One",
             itemLabel = { item -> item.toString() },
         )
