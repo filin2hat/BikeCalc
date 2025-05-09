6946467f0a0c2ea6fa0ad97a0e6a9fe259e6b7fc
Dmitriy B. <filin2hat@yandex.ru>
2025-05-09 12:21:08 +0300

    Подготовка к релизу 2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.filinhat.bikecalc"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.1.0"
+        versionCode = 3
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
