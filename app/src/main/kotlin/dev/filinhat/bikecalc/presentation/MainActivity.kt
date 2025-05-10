4175a8e2a86c21d57f5e29a66a489cc68740a09a
Dmtry Biryulin <94535311+filin2hat@users.noreply.github.com>
2025-05-10 05:42:19 +0000

    Исправление edge to edge (#25)
    Co-authored-by: Dmitriy B. <filin2hat@yandex.ru>

--- app/src/main/kotlin/dev/filinhat/bikecalc/presentation/MainActivity.kt
+++ app/src/main/kotlin/dev/filinhat/bikecalc/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
 package dev.filinhat.bikecalc.presentation
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import dev.filinhat.bikecalc.presentation.screen.main.MainScreen
 import dev.filinhat.bikecalc.presentation.ui.theme.ApplicationTheme
@@ -11,6 +14,18 @@ import dev.filinhat.bikecalc.presentation.ui.theme.ApplicationTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle =
+                SystemBarStyle.light(
+                    Color.TRANSPARENT,
+                    Color.TRANSPARENT,
+                ),
+            navigationBarStyle =
+                SystemBarStyle.light(
+                    Color.TRANSPARENT,
+                    Color.TRANSPARENT,
+                ),
+        )
         setContent {
             ApplicationTheme {
                 MainScreen()
